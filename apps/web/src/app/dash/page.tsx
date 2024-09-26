020df4b5ff832f1328c93c246d41f12910fe2931
delusionalle <antiiiclimactical@gmail.com>
2024-09-26 20:48:36 +0500

    google oauth working

---
+++ apps/web/src/app/dash/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardPage = () => {
+    return (
+        <div>
+            <h1>This is the dashboard main page hi!</h1>
+        </div>
+    );
+};
+
+export default DashboardPage;
--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthConfig } from 'next-auth'
 import Google from 'next-auth/providers/google'
+// import Resend from 'next-auth/providers/resend'
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
