250252c12bf82bfa4caf51f001e61d620449c29e
delusionalle <antiiiclimactical@gmail.com>
2024-10-08 14:19:39 +0500

    genius multiple links highlight for sidebar

--- apps/web/src/app/dash/_/sidebar-links.tsx
+++ apps/web/src/app/dash/_/sidebar-links.tsx
@@ -6,11 +6,11 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
-const links: { name: string, href: string, icon: typeof HomeIcon }[] = [
-    {name: 'Home', href: '/dash', icon: HomeIcon},
-    {name: 'List', href: '/dash/colleges', icon: ListIcon},
-    {name: 'Essays', href: '/dash/essays/', icon: FileTextIcon},
-    {name: 'LoRs', href: '/dash/letters', icon: MailsIcon},
+const links: { name: string, href: string[], icon: typeof HomeIcon }[] = [
+    {name: 'Home', href: ['/dash'], icon: HomeIcon},
+    {name: 'List', href: ['/dash/colleges'], icon: ListIcon},
+    {name: 'Essays', href: ['/dash/essays', '/dash/essays/personal'], icon: FileTextIcon},
+    {name: 'LoRs', href: ['/dash/letters'], icon: MailsIcon},
 ]
 
 function SidebarLinks() {
@@ -24,10 +24,10 @@ function SidebarLinks() {
 
                 return (
                     // TailwindCSS is so fun
-                    <Link href={link.href} key={link.name}
+                    <Link href={link.href[0]} key={link.name}
                           className={cn(`flex h-[48px] grow items-center justify-center gap-2
                           rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3`,
-                              (pathname === link.href) ? 'bg-primary text-primary-foreground' :
+                              (link.href.includes(pathname)) ? 'bg-primary text-primary-foreground' :
                                   'hover:bg-accent hover:text-accent-foreground')}>
 
                         <LinkIcon className='w-6'/>
